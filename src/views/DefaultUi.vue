c90cd8850d148dd8eb4219f5fcc3336e55229b2d
YuCh7 <aa102133395@gmail.com>
2024-11-08 21:38:17 +0800

    fix

--- src/views/DefaultUi.vue
+++ src/views/DefaultUi.vue
@@ -62,11 +62,7 @@ import MainFooter from '@/components/MainFooter.vue'
   box-sizing: border-box;
 }
 
-<<<<<<< HEAD
 @media screen and (width < 1200px) {
-=======
-@media screen and (width <= 1200px) {
->>>>>>> bfe2619a946cb5f5a6c1726dbccc7d5cdf2b102e
   .default-main-content-container {
     margin: 0px;
   }
